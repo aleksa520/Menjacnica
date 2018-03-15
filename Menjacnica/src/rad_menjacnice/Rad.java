041c723a0dd8eb4ce39d6589d8cd099ad573d2e4
aleksa520 <pavlovic1216@fon.bg.ac.rs>
2018-03-15 13:50:44 +0100

    Meotda za dodajKursValute odradjena

--- Menjacnica/src/rad_menjacnice/Rad.java
+++ Menjacnica/src/rad_menjacnice/Rad.java
@@ -1,16 +1,18 @@
 package rad_menjacnice;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.Kurs;
 import menjacnicaIS.MenjacnicaInterfejs;
 
 public class Rad implements MenjacnicaInterfejs {
 
+	LinkedList<Kurs> valute = new LinkedList<Kurs>();
 	@Override
 	public void dodajKursValute(Kurs k) {
 		// TODO Auto-generated method stub
-
+		valute.add(k);
 	}
 
 	@Override
