07c5094b0a12af2504797172e0783b39797ba7b8
aleksa520 <pavlovic1216@fon.bg.ac.rs>
2018-03-15 13:08:27 +0100

    Napravljena klasa za rad menjacnice

---
+++ Menjacnica/src/rad_menjacnice/Rad.java
@@ -0,0 +1,28 @@
+package rad_menjacnice;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.Kurs;
+import menjacnicaIS.MenjacnicaInterfejs;
+
+public class Rad implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValute(Kurs k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValute(String ime, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String vratiKurs(String ime, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
